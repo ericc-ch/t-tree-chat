975452436d21649253ee1efa772671fb1c19d474
Erick Christian <erickchristian48@gmail.com>
2025-06-09 22:49:47 +0700

    run lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css"
+import App from "./App.tsx"
+
+const root = document.querySelector("#root")
+if (!root) throw new Error("No root element found")
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
