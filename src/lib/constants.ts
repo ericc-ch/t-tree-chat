6f6e0aac0c380f707a367de4be5fbfc732d00ced
Erick Christian <erickchristian48@gmail.com>
2025-06-14 22:59:46 +0700

    tono haryadi

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,3 +7,11 @@ export const MENU_TARGET = "menuTarget"
 export const MODEL_OPTIONS: Array<ComboboxItemGroup> = [
   { group: "Google", items: GOOGLE_MODELS },
 ]
+
+export const GENERATION_CONFIG_KEYS = {
+  USER_PROMPT: "userPrompt",
+  SYSTEM_PROMPT: "systemPrompt",
+  TEMPERATURE: "temperature",
+  THINKING_MODE: "thinkingMode",
+  THINKING_BUDGET: "thinkingBudget",
+}
--- src/providers/google.ts
+++ src/providers/google.ts
@@ -1,11 +1,16 @@
-import type { ComboboxItem } from "@mantine/core"
+import type { streamText } from "ai"
+import type { FormEvent } from "react"
 
 import {
   createGoogleGenerativeAI,
   type GoogleGenerativeAIProvider,
+  type GoogleGenerativeAIProviderOptions,
 } from "@ai-sdk/google"
 import invariant from "tiny-invariant"
 
+import type { AdvancedModelSettings } from "./types"
+
+import { GENERATION_CONFIG_KEYS } from "../lib/constants"
 import { useSettingsStore } from "../stores/settings"
 
 const getGoogleProvider = () => {
@@ -18,21 +23,100 @@ const getGoogleProvider = () => {
 }
 
 type GetGoogleModelParams = Parameters<GoogleGenerativeAIProvider>
-type ModelID = GetGoogleModelParams[0]
-type ModelSettings = GetGoogleModelParams[1]
+type GoogleModelID = GetGoogleModelParams[0] | "gemini-2.5-flash-preview-05-20"
+type GoogleModelSettings = GetGoogleModelParams[1]
 
-export const getGoogleModel = (model: ModelID, settings?: ModelSettings) => {
+export const getGoogleModel = (
+  model: GoogleModelID,
+  settings?: GoogleModelSettings,
+) => {
   const provider = getGoogleProvider()
   return provider(model, settings)
 }
 
-export const GOOGLE_MODELS: Array<ComboboxItem> = [
+export const GOOGLE_MODELS: Array<{
+  value: GoogleModelID
+  label: string
+}> = [
   {
-    value: "gemini-2.0-flash-lite",
-    label: "Gemini 2.0 Flash Lite",
+    value: "gemini-2.5-flash-preview-05-20",
+    label: "Gemini 2.5 Flash Preview 05-20",
   },
   {
     value: "gemini-2.0-flash",
     label: "Gemini 2.0 Flash",
   },
+  {
+    value: "gemini-2.0-flash-lite",
+    label: "Gemini 2.0 Flash Lite",
+  },
 ]
+
+export const GOOGLE_MODEL_AVAILABLE_SETTINGS = new Map<
+  GoogleModelID,
+  AdvancedModelSettings
+>([
+  [
+    "gemini-2.5-flash-preview-05-20",
+    {
+      systemPrompt: true,
+      temperature: true,
+      thinkingMode: true,
+      thinkingBudget: true,
+    },
+  ],
+  [
+    "gemini-2.0-flash",
+    {
+      systemPrompt: true,
+      temperature: true,
+      thinkingMode: false,
+      thinkingBudget: false,
+    },
+  ],
+  [
+    "gemini-2.0-flash-lite",
+    {
+      systemPrompt: true,
+      temperature: true,
+      thinkingMode: false,
+      thinkingBudget: false,
+    },
+  ],
+])
+
+type StreamTextOptions = Parameters<typeof streamText>[0]
+type OptionsParser = (event: FormEvent<HTMLFormElement>) => StreamTextOptions
+
+export const GOOGLE_MODEL_OPTIONS_PARSERS: Map<GoogleModelID, OptionsParser> =
+  new Map([
+    [
+      "gemini-2.5-flash-preview-05-20",
+      (event): StreamTextOptions => {
+        const formData = new FormData(event.currentTarget)
+
+        const prompt = formData.get(
+          GENERATION_CONFIG_KEYS.USER_PROMPT,
+        ) as string
+        const temperatureString = formData.get(
+          GENERATION_CONFIG_KEYS.TEMPERATURE,
+        ) as string
+        const temperature = Number.parseFloat(temperatureString)
+
+        return {
+          prompt: formData.get("prompt") as string,
+          system: formData.get("systemPrompt") as string,
+          temperature,
+          providerOptions: {
+            google: {
+              thinkingConfig: {
+                includeThoughts,
+              },
+            } satisfies GoogleGenerativeAIProviderOptions,
+          },
+        }
+      },
+    ],
+  ])
+
+const tont: Parameters<typeof streamText>[0] = {}
